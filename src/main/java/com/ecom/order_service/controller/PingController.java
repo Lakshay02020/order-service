8acafd03622ec2599cedf14b5e3347dc940058d5
Lakshay Singla <lakshay02singla@gmail.com>
2025-07-24 11:21:44 +0530

    ENable scheduling

--- src/main/java/com/ecom/order_service/controller/PingController.java
+++ src/main/java/com/ecom/order_service/controller/PingController.java
@@ -1,7 +1,6 @@
 package com.ecom.order_service.controller;
 
-import com.ecom.order_service.feign.CartFeignProvider;
-import com.ecom.order_service.feign.ProductFeignProvider;
+import com.ecom.order_service.feign.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,15 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class PingController {
 
-
-    // TODO understand volatile meaning here
     private volatile boolean isPingJobEnabled = true;
 
-    @Autowired
-    CartFeignProvider cartFeignProvider;
-
-    @Autowired
-    ProductFeignProvider productFeignProvider;
+    @Autowired private CartFeignProvider cartFeignProvider;
+    @Autowired private ProductFeignProvider productFeignProvider;
+    @Autowired private PaymentFeignProvider paymentFeignProvider;
+    @Autowired private UserFeignProvider userFeignProvider;
+    @Autowired private EmailFeignProvider emailFeignProvider;
 
     @PostMapping("/ping-toggle")
     public void togglePing(@RequestParam boolean enable) {
@@ -31,19 +28,31 @@ public class PingController {
         log.info("Ping job toggled to: {}", enable);
     }
 
-    @Scheduled(fixedRate = 7 * 60 * 1000)
-    public void pingProductService() {
+    @Scheduled(fixedRate = 7 * 60 * 1000) // every 7 minutes
+    public void pingAllServices() {
         if (!isPingJobEnabled) {
             log.debug("Ping job is disabled.");
             return;
         }
 
+        log.info("Starting scheduled ping to all services...");
+
+        ping("Product Service", () -> productFeignProvider.ping());
+        ping("Cart Service", () -> cartFeignProvider.ping());
+        ping("Payment Service", () -> paymentFeignProvider.ping());
+        ping("User Service", () -> userFeignProvider.ping());
+        ping("Email Service", () -> emailFeignProvider.ping());
+
+        log.info("Completed scheduled ping to all services.");
+    }
+
+    private void ping(String serviceName, Runnable pingAction) {
         try {
-            log.info("Pinging product service...");
-            productFeignProvider.ping();
-            log.info("Ping to product service successful.");
+            log.info("Pinging {}...", serviceName);
+            pingAction.run();
+            log.info("Ping to {} successful.", serviceName);
         } catch (Exception e) {
-            log.error("Error occurred while pinging product service: {}", e.getMessage(), e);
+            log.error("Ping to {} failed: {}", serviceName, e.getMessage(), e);
         }
     }
 }
